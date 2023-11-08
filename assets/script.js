b2ad7dc4ddaab3f3c5cfacae3fed49709b03f204
Sarahy M <sarahym@Sarahys-MBP.the-beach.net>
2023-11-08 13:09:27 -0500

    added more javascript, but link still not working

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,31 @@
-var APIKey = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=4e6e807ee59834126e8fdbcfad716167";
+var APIKey = "3484fb7b5febe1605342141d4f9ba608"
+
+document.getElementById('search-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    var city = document.getElementById('search-input').value;
+  
+
+    //https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+    fetch(queryURL)
+      .then(response => response.json())
+      .then(data => {
+
+        document.getElementById('result-text').textContent = city;
+  
+        var currentWeather = `
+          <h3>Current Weather</h3>
+          <p>Temperature: ${data.main.temperature} °C</p>
+          <p>Humidity: ${data.main.humidity} %</p>
+          <p>Wind Speed: ${data.wind.speed} m/s</p>
+        `;
+        document.getElementById('result-content').innerHTML = currentWeather;
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('result-content').innerHTML = '<p>Error fetching weather data</p>';
+      });
+  });
+  
